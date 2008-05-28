ada7edb03c3da1f0f524a327cf2cb0b09c5bc9d3
mauriciobomfim <mauriciobomfim@16b7644a-2004-43b6-96b6-3579740b01cb>
2008-05-28 21:22:12 +0000

    git-svn-id: file:///home/mauricio/Downloads/repo.svndump/svn/trunk@3 16b7644a-2004-43b6-96b6-3579740b01cb

--- activerdf/test/objectmanager/test_resource_writing.lua
+++ activerdf/test/objectmanager/test_resource_writing.lua
@@ -10,7 +10,7 @@ function setup()
 end
 
 function test_update_value()
-	Namespace.register('a'r, 'http://activerdf.org/test/')
+	Namespace.register('ar', 'http://activerdf.org/test/')
 	local adapter = get_write_adapter()
 
 	local eyal = RDFS.Resource ( 'http://activerdf.org/test/eyal' )
@@ -18,7 +18,7 @@ function test_update_value()
 
 	eyal.ar.age = 100
 	assert ( 100 == eyal.ar.age )
-	assert ( activerdf.table.equals ( { 100 }, eyal.all_ar.age 
+	assert ( activerdf.table.equals ( { 100 }, eyal.all_ar.age ) )
 
 	eyal.ar.age = { 100, 80 }
 	assert ( activerdf.table.equals ( { 100, 80 }, eyal.ar.age ) )
